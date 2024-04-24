0de340527a20c75db865077a942fda229568cacc
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-04-25 00:39:02 +0300

    add Lesson_4

---
+++ Lesson_4/program.java
@@ -0,0 +1,65 @@
+package Lesson_4;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class program {
+        // LinkedList
+
+    // public static void main(String[] args) {
+    //     Character value = null;
+    //     LinkedList<Integer> ll = new LinkedList<Integer>();
+    //     ll.add(1);
+    //     ll.add(2);
+    //     ll.add(3);
+    //     System.out.println(ll);
+    // }
+
+
+
+
+        // Queue
+    // public static void main(String[] args) {
+    //     Queue<Integer> queue = new LinkedList<Integer>();
+    //     queue.add(123);
+    //     queue.add(3);
+    //     queue.add(13);
+    //     queue.add(1);
+
+    //     System.out.println(queue);
+
+
+        // PriorityQueue
+    // public static void main(String[] args) {
+    //     PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+    //     pq.add(123);
+    //     pq.add(3);
+    //     pq.add(13);
+    //     pq.add(1);
+
+    //     System.out.println(pq);
+    //     System.out.println(pq.poll()); // приоритетность удаления (в первую очередь наименьшее число)
+    //     System.out.println(pq.poll());
+    //     System.out.println(pq.poll());
+
+
+    // Deque
+    // public static void main(String[] args) {
+    //     Deque<Integer> deque = new ArrayDeque<>();
+    //     // Deque<Integer> deque = new ArrayDeque<>();
+    //     deque.addFirst(1); deque.addLast(2);
+    //     deque.removeLast(); deque.removeLast();
+    //     deque.offerFirst(1); deque.offerLast(2);
+    //     deque.pollFirst(); deque.pollLast();
+    //     // deque.getFirst(); deque.getLast();
+    //     deque.peekFirst(); deque.peekLast();
+        
+        }
+       
+      
+    }
+       
+// }
